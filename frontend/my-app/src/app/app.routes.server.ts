52e2166b3b825f9fc71452d98db68e2057e7563f
BasharQamhiyeh <bashar.qamhiyeh@gmail.com>
2025-10-22 20:08:47 +0200

    fix issue in building app

--- frontend/my-app/src/app/app.routes.server.ts
+++ frontend/my-app/src/app/app.routes.server.ts
@@ -1,8 +1,24 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ServerRoute, RenderMode } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // For static home page
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  // For item-details with :id — disable prerender, or provide getPrerenderParams
+  {
+    path: 'item-details/:id',
+    renderMode: RenderMode.Server
+  },
+  // For admin categories/:id
+  {
+    path: 'admin/categories/:id',
+    renderMode: RenderMode.Server
+  },
+  // you may add a fallback
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
