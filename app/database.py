13f9186476917bab763726649b202d21f6a81951
BasharQamhiyeh <bashar.qamhiyeh@gmail.com>
2025-10-22 20:25:26 +0200

    use env variable in backend for database config

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:admin@localhost:5432/marketplace_db"
+user = os.environ.get('DB_USER', 'admin')
+password = os.environ.get('DB_PASSWORD', 'postgres')
+name = os.environ.get('DP_NAME','marketplace_db')
+host = os.environ.get('DB_HOST', 'localhost:5432')
+
+
+DATABASE_URL = f"postgresql+asyncpg://{password}:{user}@{host}/{name}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
